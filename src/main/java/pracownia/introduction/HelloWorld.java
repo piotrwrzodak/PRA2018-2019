35d8bcbec9dd19d9b90b75894184c3b896506ca2
s444510 <piotrwrzodakk@gmail.com>
2019-10-07 12:58:01 +0200

    pierwszy commit

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,18 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+        Logger log = Logger.getLogger("sd");
+
+        log.info("dshsa");
+        log.error("wffwe");
     }
+
+
+
 }
